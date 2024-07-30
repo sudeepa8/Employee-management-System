e06a36bf0306cdda890d09135b7ad46b2861bfcc
sudeepa wickramarachci <sudeepawicramarachchi8@gmail.com>
2024-07-30 11:07:21 +0530

    Update PasswordResetTokenRepository.java
    @Repository

--- repository/PasswordResetTokenRepository.java
+++ repository/PasswordResetTokenRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
   Optional<PasswordResetToken> findByToken(String token);
 }
